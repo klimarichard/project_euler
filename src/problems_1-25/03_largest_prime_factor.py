99cf9686159c00ac2633141092890c80235d86b1
klimarichard <klima.risa@gmail.com>
2020-11-13 01:14:10 +0100

    Updated problem 3

--- src/problems_1-25/03_largest_prime_factor.py
+++ src/problems_1-25/03_largest_prime_factor.py
@@ -1,23 +1,4 @@
-from algorithms import divisors
+from algorithms import prime_factors
 
 
-def find_largest_prime_factor(n):
-    """
-    Find the largest prime factor of given number.
-    :param n: an integer
-    :return: largest prime factor of n
-    """
-    divs = divisors(n)
-
-    current_divs = divs.copy()
-    i = len(divs) - 2
-
-    while len(current_divs) > 2:
-        current = divs[i]
-        current_divs = [x for x in divs if current % x == 0]
-        i -= 1
-
-    return current_divs[1]
-
-
-print(find_largest_prime_factor(600851475143))
+print(max(prime_factors(600851475143)))
