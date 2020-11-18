da7295b71632b20dbe84ed8d68520a9c29cf5ce9
klimarichard <klima.risa@gmail.com>
2020-11-18 19:29:18 +0100

    Optimized solution for Problem 9

--- src/problems_1-25/09_special_pythagorean_triplet.py
+++ src/problems_1-25/09_special_pythagorean_triplet.py
@@ -4,8 +4,16 @@ def pythagorean_triplets_sum(n):
     :param n: desired sum of the triplet
     :return: list of Pythagorean triplets satisfying the condition
     """
-    return [(a, b, c) for a in range(1001) for b in range(a, 1001) for c in range(1, a + b)
-            if a * a + b * b == c * c and a + b + c == 1000]
+    # we can use several general properties of triangles:
+    #  - triangle inequality (mainly c < a + b)
+    #
+    # as well as some properties of primitive Pythagorean triplets:
+    #  - exactly one of a, b is odd (we say a is odd, b is even)
+    #  - exactly one of a, b is divisible by 3 (for us, a is divisible by 3)
+    #  - exactly one of a, b is divisible by 4 (for us, b is divisible by 4)
+    #  - c is of the form 4n + 1
+    return [(a, b, n - (a + b)) for a in range(3, n + 1, 3) for b in range(4, n + 1 - (a + 1), 4)
+            if (a % 2 != b % 2) and a * a + b * b == ((n - (a + b)) * (n - (a + b)))]
 
 
 def triplet_products(triplets):
