3aebef14bf5555b1b01906996c292494b6c62753
klimarichard <klima.risa@gmail.com>
2020-11-18 10:54:02 +0100

    Generalized finding function in solution to Problem 69

--- src/problems_51-75/69_totient_maximum.py
+++ src/problems_51-75/69_totient_maximum.py
@@ -1,10 +1,12 @@
 from algorithms import prime_factors_list
 
 
-def find_maximal_totient_ratio(n):
+def find_totient_ratio(n, kind='max'):
     """
     Find a number n in given range for which the ratio n/φ(n) is maximal.
     :param n: upper bound
+    :param kind: optional parameter (default = 'max') determining, whether
+                 a maximum or minimum ratio should be returned
     :return: a number with maximal ratio
     """
     # by definition of Euler's phi function, it holds:
@@ -12,17 +14,31 @@ def find_maximal_totient_ratio(n):
     #        * p_r^(k_r - 1) * (p_r - 1), for
     # n = p_1^(k_1) * p_2^(k_2) * ... * p_r^(k_r), where
     # p_1, p_2, ..., p_r are distinct primes, that divide n
-    max_ratio = 1
-    max_n = 1
+    if kind == 'max':
+        max_ratio = 1
+        max_n = 1
+    elif kind == 'min':
+        min_ratio = n + 1
+        min_n = 1
+    else:
+        return None
 
     for k in range(2, n + 1):
         phi = totient(k)
 
-        if k / phi > max_ratio:
-            max_ratio = k / phi
-            max_n = k
+        if kind == 'max':
+            if k / phi > max_ratio:
+                max_ratio = k / phi
+                max_n = k
+        if kind == 'min':
+            if k / phi < min_ratio:
+                min_ratio = k / phi
+                min_n = k
 
-    return max_n
+    if kind == 'max':
+        return max_n
+    else:
+        return min_n
 
 
 def totient(n):
@@ -40,4 +56,4 @@ def totient(n):
     return phi
 
 
-print(find_maximal_totient_ratio(1000000))
+print(find_totient_ratio(1000000))
