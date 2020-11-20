8611ebb27edc486417afae679a290258ee68c8d1
klimarichard <klima.risa@gmail.com>
2020-11-20 19:38:45 +0100

    Updated solution to Problem 77

--- src/problems_76-100/77_prime_summations.py
+++ src/problems_76-100/77_prime_summations.py
@@ -1,28 +1,33 @@
 from algorithms import eratosthenes
 
 
-def find_primes_summations(n):
+def find_primes_summations(k):
     """
-    Finds how many ways can numbers up to given integer be written as a sum of
-    at least two primes.
-    :param n: an integer
-    :return: number of ways to write numbers 1 to n as a summation of primes
+    Finds the first number, that can be written as a sum of at least two primes
+    more than k ways.
+    :param k: lower limit for ways
+    :return: first number, that can be written more than k ways
     """
-    primes = eratosthenes(n)
+    primes = eratosthenes(100)
+    n = 2
+    flag = True
 
-    # initialize list for dynamic programming
-    ways = [0 for _ in range(n + 1)]
-    ways[0] = 1
+    while flag:
+        # initialize list for dynamic programming
+        ways = [0 for _ in range(n + 1)]
+        ways[0] = 1
 
-    # compute elements of the list
-    for p in primes:
-        for j in range(p, n + 1):
-            ways[j] += ways[j - p]
+        # compute elements of the list
+        for p in primes:
+            for j in range(p, n + 1):
+                ways[j] += ways[j - p]
 
-    return ways
+        if ways[n] > k:
+            flag = False
+        else:
+            n += 1
+
+    return n
 
 
-# 100 was chosen arbitrarily, but I couldn't find it analytically
-prime_sums = find_primes_summations(100)
-
-print(prime_sums.index(list(filter(lambda x: x > 5000, prime_sums))[0]))
+print(find_primes_summations(5000))
