9dd0eaa6a6dbacbd675a3bd89bdf82116f2008f2
klimarichard <klima.risa@gmail.com>
2020-11-22 15:41:09 +0100

    Better and faster solution to Problem 81

--- src/problems_76-100/81_path_sum_two_ways.py
+++ src/problems_76-100/81_path_sum_two_ways.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def dijkstra_algorithm(matrix):
+def minimal_top_bottom_path(matrix):
     """
     Computes the shortest path from the top left to the bottom right corner
     of the matrix when moving only down or to the right with Dijkstra's
@@ -9,41 +9,20 @@ def dijkstra_algorithm(matrix):
     :param matrix: a matrix with values on the path
     :return: length of the shortest path from top left to bottom right
     """
-    # initializing value of "infinity" as the sum of all costs in the graph
-    infinity = sum([v for row in matrix for v in row])
+    matrix = np.array(matrix)
 
-    # initialize Dijkstra's algorithm
-    distance = np.full(shape=(len(matrix), len(matrix[0])),
-                       fill_value=infinity,
-                       dtype=int)
-    distance[0, 0] = matrix[0][0]
-    # active vertices
-    A = {(i, j) for i in range(distance.shape[0]) for j in range(distance.shape[1])}
+    # for each column, find the row in the previous column
+    # which is the cheapest to get here from, so we compute
+    # the solution from back to front
+    for i in range(matrix.shape[1] - 2, -1, -1):
+        matrix[matrix.shape[1] - 1, i] += matrix[matrix.shape[1] - 1, i + 1]
+        matrix[i, matrix.shape[1] - 1] += matrix[i + 1, matrix.shape[1] - 1]
 
-    # we stop once we found the shortest path to the bottom right corner
-    while (distance.shape[0] - 1, distance.shape[1] - 1) in A:
-        # 1) find graphs with minimal distance so far
-        min_distance = infinity
-        for v in A:
-            current = distance[v]
+    for i in range(matrix.shape[1] -2, -1, -1):
+        for j in range(matrix.shape[1] - 2, -1, -1):
+            matrix[i, j] += min(matrix[i + 1, j], matrix[i, j + 1])
 
-            if current < min_distance:
-                min_distance = current
-
-        # 2) find all vertices with minimal distance so far
-        N = {v for v in A if distance[v] == min_distance}
-        A -= N
-
-        # 3) actualize values for neighbours of v in N
-        for v in N:
-            if v[0] + 1 < distance.shape[0]:
-                distance[v[0] + 1, v[1]] = min(distance[v[0] + 1, v[1]],
-                                               distance[v] + matrix[v[0] + 1][v[1]])
-            if v[1] + 1 < distance.shape[1]:
-                distance[v[0], v[1] + 1] = min(distance[v[0], v[1] + 1],
-                                               distance[v] + matrix[v[0]][v[1] + 1])
-
-    return distance[-1, -1]
+    return matrix[0, 0]
 
 
 matrix = []
@@ -55,4 +34,4 @@ with open('../../res/p081_matrix.txt', 'r') as f:
         matrix.append(list(map(int, line.strip().split(','))))
         line = f.readline()
 
-print(dijkstra_algorithm(matrix))
+print(minimal_top_bottom_path(matrix))
