523234de5855d1d0b3d8f29df30bab5f6458a207
klimarichard <klima.risa@gmail.com>
2020-11-23 14:44:14 +0100

    Updated solution to Problem 2

--- src/problems_1-25/02_even_fibonacci_numbers.py
+++ src/problems_1-25/02_even_fibonacci_numbers.py
@@ -1,14 +1,23 @@
 from algorithms import gen_fibs
 
 
-f = gen_fibs()
-next_fib = next(f)
-sum = 0
-
-while next_fib < 4000000:
-    if next_fib % 2 == 0:
-        sum += next_fib
-
+def sum_of_even_fibs(n):
+    """
+    Returns sum of all even Fibonacci numbers up to given bound.
+    :param n: upper bound
+    :return: sum of even Fibonacci numbers lesser than n
+    """
+    f = gen_fibs()
     next_fib = next(f)
+    sum = 0
 
-print(sum)
+    while next_fib < n:
+        if next_fib % 2 == 0:
+            sum += next_fib
+
+        next_fib = next(f)
+
+    return sum
+
+
+print(sum_of_even_fibs(4000000))
