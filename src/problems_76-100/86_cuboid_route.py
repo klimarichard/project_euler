93c2f8d75101299f237976ba0fc026881b023c0c
klimarichard <klima.risa@gmail.com>
2020-11-23 12:19:44 +0100

    Generalized solution to Problem 86

--- src/problems_76-100/86_cuboid_route.py
+++ src/problems_76-100/86_cuboid_route.py
@@ -2,6 +2,33 @@
 # only on the surfaces for cuboid with dimensions a <= b <= c is
 # d = sqrt(c^2 + (a + b)^2)
 
+def smallest_cuboid_with_k_routes(k):
+    """
+    Finds the first M, such that a cuboid of maximum size M × M × M contains
+    at least k integer shortest routes from one corner to the opposite one.
+    :param k: lower limit for shortest integer routes
+    :return: first dimension satisfying k shortest integer routes
+    """
+    c = 2
+    routes = 0
+
+    while routes < k:
+        c += 1
+        for ab in range(3, 2 * c + 1):
+            current = (c * c + ab * ab) ** 0.5
+            # epsilon value is arbitrary
+            if equal(current, int(current), 0.0001):
+                # determines how many combinations of a + b we can get
+                if ab <= c:
+                    # a = 1, b = ab - 1, or a = 2, b = ab - 2, etc.
+                    routes += (ab // 2)
+                else:
+                    # b between upper whole part of (a + b) / 2 included and c included
+                    routes += (1 + (c - (ab + 1) // 2))
+
+    return c
+
+
 def distance_to_opposite_corner(a, b, c):
     """
     Computes distance from one corner of the cuboid to the opposite one
@@ -29,21 +56,4 @@ def equal(a, b, epsilon):
         return False
 
 
-c = 2
-routes = 0
-
-while routes < 1000000:
-    c += 1
-    for ab in range(3, 2 * c + 1):
-        current = (c * c + ab * ab) ** 0.5
-        # epsilon value is arbitrary
-        if equal(current, int(current), 0.0001):
-            # determines how many combinations of a + b we can get
-            if ab <= c:
-                # a = 1, b = ab - 1, or a = 2, b = ab - 2, etc.
-                routes += (ab // 2)
-            else:
-                # b between upper whole part of (a + b) / 2 included and c included
-                routes += (1 + (c - (ab + 1) // 2))
-
-print(c)
+print(smallest_cuboid_with_k_routes(1000000))
