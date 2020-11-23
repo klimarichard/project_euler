0119badf57c35517f706732997bfa86ee6454dbb
klimarichard <klima.risa@gmail.com>
2020-11-23 12:16:38 +0100

    Generalized solution to Problem 85

--- src/problems_76-100/85_counting_rectangles.py
+++ src/problems_76-100/85_counting_rectangles.py
@@ -4,6 +4,43 @@
 # we can therefore try all combinations of N and M up to
 # 2000000 rectangles in the grid and find the solution this way
 
+def nearest_rectangles(k):
+    """
+    Finds a grid for which the number of rectangles in it is the closest
+    to given number.
+    :param k: an integer
+    :return: n, m dimensions with the closest number of rectangles
+    """
+    threshold = k ** 0.5
+
+    n = 1
+    m = 1
+    closest = k
+    closest_nm = n, m
+    rectangles = rect(n, m)
+    flag = True
+
+    while flag:
+        while rectangles <= k:
+            current = abs(k - rectangles)
+            if current < closest:
+                closest = current
+                closest_nm = n, m
+
+            n += 1
+            rectangles = rect(n, m)
+
+        m += 1
+        n = m
+        rectangles = rect(n, m)
+
+        # we would only get higher numbers from now on
+        if m > threshold and n > threshold:
+            flag = False
+
+    return closest_nm
+
+
 def rect(n, m):
     """
     Computes number of rectangles in an n × m grid
@@ -14,31 +51,5 @@ def rect(n, m):
     return (m * (m + 1) * n * (n + 1)) // 4
 
 
-threshold = 2000000 ** 0.5
-
-n = 1
-m = 1
-closest = 2000000
-closest_nm = n, m
-rectangles = rect(n, m)
-flag = True
-
-while flag:
-    while rectangles <= 2000000:
-        current = abs(2000000 - rectangles)
-        if current < closest:
-            closest = current
-            closest_nm = n, m
-
-        n += 1
-        rectangles = rect(n, m)
-
-    m += 1
-    n = m
-    rectangles = rect(n, m)
-
-    # we would only get higher numbers from now on
-    if m > threshold and n > threshold:
-        flag = False
-
-print(closest_nm[0] * closest_nm[1])
+closest = nearest_rectangles(2000000)
+print(closest[0] * closest[1])
